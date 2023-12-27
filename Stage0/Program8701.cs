4eced8b0729336267baafe04a2f037651f6301a0
Yifat21 <yifatoriel@gmail.com>
2023-12-27 18:00:08 +0200

    Initial Code+Welcome function

---
+++ Stage0/Program8701.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stage0
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome8701();
+            Welcome7640();
+            Console.ReadKey();
+        }
+
+        private static void Welcome8701()
+        {
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcom to my first console application");
+        }
+
+        static partial void Welcome7640();
+        {
+
+        }
+    }
+}
+
