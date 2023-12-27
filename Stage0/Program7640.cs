fe18e01e52fe46c20b10bc3991b437e2f6f2ef1b
naomiBelenky <nbelenky8@gmail.com>
2023-12-27 18:26:46 +0200

    my first try

---
+++ Stage0/Program7640.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    internal partial class Program
+    {
+        static partial void Welcome7640()
+        {
+            Console.WriteLine("I love chocolate");
+        }
+    }
+}
