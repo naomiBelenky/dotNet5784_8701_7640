d10e7de60dba7f043a6d0938c1f3d888133009d6
Yifat21 <yifatoriel@gmail.com>
2024-01-08 16:07:45 +0200

    stage 1

--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -5,22 +5,22 @@ using System.Globalization;
 namespace DO;
 
 /// <summary>
-/// Task Entity represents a task with all its props
+/// Task entity represents a task with all props.
 /// </summary>
-/// <param name="TaskID"> Personal unique ID of the task </param>
-/// <param name="Name"> The task's name </param>
-/// <param name="Description"> The task's description </param>
-/// <param name="Milestone"> Describes whether the task is complete </param>
-/// <param name="Creation"> The date of the task's creation </param>
-/// <param name="PlanToStart"> The date of starting according to plan  </param>
-/// <param name="StartWork"> the date of starting work </param>
-/// <param name="TimeForTask"> the ammount of days that needed for the task  </param>
-/// <param name="Deadline"></param>
-/// <param name="FinishDate"></param>
-/// <param name="Product"></param>
-/// <param name="Notes"></param>
-/// <param name="EngineerID"></param>
-/// <param name="Difficulty"></param>
+/// <param name="TaskID">Personal unique ID of the task</param>
+/// <param name="TaskName">The task's name</param>
+/// <param name="Description">The task's description</param>
+/// <param name="Milestone">Describes wether the task is complete</param>
+/// <param name="Creation">The date that the task was created by the manager</param>
+/// <param name="PlanToStart">The planned date to start working on the task</param>
+/// <param name="StartWork">The actual date of starting working on the task</param>
+/// <param name="TimeForTask">Amount of days that are needed to work on the task</param>
+/// <param name="Deadline">Deadline of the task</param>
+/// <param name="FinishDate">Actual date of finishing the task</param>
+/// <param name="Product">Describes the results or provided items of the finished task</param>
+/// <param name="Notes">Remarks and notes about the task or products</param>
+/// <param name="EngineerId">The ID of the assigned engineerfor the task</param>
+/// <param name="Difficulty">The level of difficulty of the task, defines the level of expertise needed to work on it</param>
 
 public record Task
 (
