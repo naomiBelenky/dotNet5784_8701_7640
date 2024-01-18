8f96f5ddb7cf97a6f41cd866fa832fb412f9e199
Yifat21 <yifatoriel@gmail.com>
2024-01-18 13:49:04 +0200

    Stage 2 ch 5

--- DalXml/EngineerImplementation.cs
+++ DalXml/EngineerImplementation.cs
@@ -14,12 +14,40 @@ internal class EngineerImplementation:IEngineer
         if (Read(item.EngineerID) is not null) //If this id already exist
             throw new DalAlreadyExistsException($"Engineer with ID={item.EngineerID} already exist!!");
 
-        XMLTools.SaveListToXMLElement();
+        XElement engineerList = XMLTools.LoadListFromXMLElement(s_engineers_xml);
+
+        XElement newItem = new XElement("Engineer");
+
+        newItem.Add(new XElement("EngineerID", item.EngineerID));
+        newItem.Add(new XElement("FullName", item.FullName));
+        newItem.Add(new XElement("Email", item.Email));
+        newItem.Add(new XElement("Level", item.Level));
+        if(item.CostPerHour!=null)
+            newItem.Add(new XElement("CostPerHour", item.CostPerHour));
+
+        engineerList.Add(newItem);
+        XMLTools.SaveListToXMLElement(engineerList, s_engineers_xml);
+
+        return item.EngineerID;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+       //Engineer? temp = Read(id);
+        if (Read(id) == null) //If this id doesnt exist
+            throw new DalDoesNotExistException($"Engineer with ID={id} does Not exist");
+
+
+        XElement engineerList = XMLTools.LoadListFromXMLElement(s_engineers_xml);
+
+        XElement? temp= (from p in engineerList.Elements()
+         where Convert.ToInt32(p.Element("id").Value) == id
+         select p).FirstOrDefault();
+
+        if (temp != null)   
+            temp.Remove();
+
+        XMLTools.SaveListToXMLElement(engineerList, s_engineers_xml);
     }
 
     public Engineer? Read(Func<Engineer, bool> filter)
@@ -39,6 +67,12 @@ internal class EngineerImplementation:IEngineer
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        Engineer? temp = Read(item.EngineerID);
+        if (temp == null) //If this id doesnt exist
+            throw new DalDoesNotExistException($"Engineer with ID={item.EngineerID} does Not exist");
+
+        XElement engineerList = XMLTools.LoadListFromXMLElement(s_engineers_xml);
+        Delete(item.EngineerID);
+        Create(item); 
     }
 }
