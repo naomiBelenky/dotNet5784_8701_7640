df3258c6493e5ba262b9f216316ae2c1450c61f4
naomiBelenky <nbelenky8@gmail.com>
2024-01-18 21:51:45 +0200

    stage 3 until chapter 6

---
+++ DalXml/DalXml .cs
@@ -0,0 +1,12 @@
+﻿using DalApi;
+
+namespace Dal;
+
+public class DalXml : IDal
+{
+    public ITask Task => new TaskImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public ILink Link => new LinkImplementation();
+}
