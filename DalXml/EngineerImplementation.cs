56287c32f9c1a5f1decad48aea1ca2cb011f9d72
Yifat21 <yifatoriel@gmail.com>
2024-01-21 12:52:49 +0200

    one trying to fix "GetEngineer"

--- DalXml/EngineerImplementation.cs
+++ DalXml/EngineerImplementation.cs
@@ -90,7 +90,9 @@ internal class EngineerImplementation : IEngineer
             EngineerID = Convert.ToInt32(eng.Element("EngineerID").Value),
             FullName = eng.Element("FullName").Value,
             Email = eng.Element("Email").Value,
-            Level = (EngineerLevel)Convert.ToInt32(eng.Element("Level").Value),
+            //Level = (EngineerLevel)Convert.ToInt32(eng.Element("Level").Value),
+            Level = Enum.Parse<EngineerLevel>(eng.Element("Level").Value),
+
             CostPerHour = Convert.ToDouble(eng.Element("CostPerHour").Value)
         };
     }
