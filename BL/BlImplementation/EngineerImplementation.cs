243103903d9ac93c2fc44c32c47ca15228dd7127
Yifat21 <yifatoriel@gmail.com>
2024-01-28 08:47:08 +0200

    stage 4 chapter with "read"

--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -1,9 +1,9 @@
 ﻿
 
 using BlApi;
-using DO;
-using System.Collections;
+using BO;
 using System.Collections.Generic;
+
 
 
 
@@ -25,7 +25,7 @@ internal class EngineerImplementation : IEngineer
             }
             else throw new BlDoesNotExistException("id or name or cost or email doesnt valid");
         }
-        catch(DO.DalAlreadyExistsException messege)
+        catch (DO.DalAlreadyExistsException messege)
         {
             throw new BlDoesNotExistException($"Engineer with ID={item.Id} already exists", messege);
         }
@@ -37,39 +37,41 @@ internal class EngineerImplementation : IEngineer
 
     public void Delete(int id)
     {
-        DO.Engineer? doEng = _dal.Engineer.Read(id);
-        if (doEng == null)
-            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
 
-        return new BO.Engineer()
-        {
-            Id = id,
-            Name = doEng.FullName,
-            Email = doEng.Email,
-            Level = (BO.EngineerLevel)doEng.Level,
-            Cost = doEng.CostPerHour,
-            //IEnumerable<DO.Task?> temp = from item in _dal.Task.ReadAll()
-            //                             where id == item.EngineerID
-            //                             select item;
-            IEnumerable<DO.Task> temp = _dal.Task.ReadAll(item => item.EngineerID == id);
+    }
 
-    
-        };
 
-    
+
 
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Engineer? doEng = _dal.Engineer.Read(id); //קוראים את הנתונים
+        if (doEng == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+
+        BO.Engineer eng = new Engineer() //יוצרים ישות לוגית בעזרתם
+        { Id = id, Name = doEng.FullName, Email = doEng.Email,
+            Level = (BO.EngineerLevel)doEng.Level, Cost = doEng.CostPerHour };
+
+        var task = _dal.Task.Read(item => item.EngineerID == id &&
+           item.StartWork < DateTime.Now && DateTime.Now < item.Deadline); //משתנה עזר שבודק האם יש משימה 
+
+        if(task!=null) //אם יש אז נעדכן אותה 
+        { 
+            BO.TaskInEngineer temp=new BO.TaskInEngineer() { Id= task.TaskID, Name= task.Name };
+            eng.Task = temp;
+        }
+
+        return eng;
     }
 
     public IEnumerable<Engineer> ReadAll(Func<bool> filter)
-    {
-        throw new NotImplementedException();
-    }
+{
+    throw new NotImplementedException();
+}
 
     public void Update(Engineer engineer)
-    {
-        throw new NotImplementedException();
-    }
+{
+    throw new NotImplementedException();
+}
 }
