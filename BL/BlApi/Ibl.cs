a35be821c2fca47580edc4d0093187a24d814896
Yifat21 <yifatoriel@gmail.com>
2024-02-05 11:06:36 +0200

    check2

--- BL/BlApi/Ibl.cs
+++ BL/BlApi/Ibl.cs
@@ -1,15 +1,9 @@
-﻿using BO;
+﻿namespace BlApi;
 
-namespace BlApi;
-
-<<<<<<<< HEAD:BL/BlApi/IBL.cs
-public interface IBL
-========
 public interface IBl
->>>>>>>> 78ab7731c13642ddec7e88664360f1e0ca0c0637:BL/BlApi/IBl.cs
 {
+    //static Stage stage { get; }
     public IEngineer Engineer { get; }
     public ITask Task { get; }
     public IMilestone MileStone { get; }
-    static Stage Stage { get;  }
 }
