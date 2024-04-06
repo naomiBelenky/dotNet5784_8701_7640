6bcf6994f34e1d7a31da68c45f2bedf351e4e1f1
Yifat21 <yifatoriel@gmail.com>
2024-04-06 20:46:40 +0300

    with Func

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -5,10 +5,9 @@ using System;
 sealed internal class DalList : IDal
 {
     public static IDal Instance { get; } = new DalList();
+
     public ITask Task => new TaskImplementation();
-
     public IEngineer Engineer => new EngineerImplementation();
-
     public ILink Link => new LinkImplementation();
 
     //public DateTime? StartDate { get => Instance.StartDate; set => Instance.StartDate=value; }
